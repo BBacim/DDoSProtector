69858e58dd5de0a325e30af3d2249560f0b7ef34
Bacim Boulehmi <33480115+BBacim@users.noreply.github.com>
2022-11-05 14:30:45 +0100

    Express Server initialization

---
+++ app.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+// Express inisialization
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require("ejs");
+
+const port = process.env.PORT || 3000;
+
+var routes = require('./routes');
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(routes);
+
+app.listen(port, function () {
+  console.log('Server started on port ', port);
+});
